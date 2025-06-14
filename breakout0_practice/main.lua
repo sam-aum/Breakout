6788b648f11a11adfd1042364107fae18bd4e901
Sam Aum <psaum01@gmail.com>
2025-06-14 16:30:10 -0500

    practice

--- breakout0_practice/main.lua
+++ breakout0_practice/main.lua
@@ -1,6 +1,102 @@
-require src/Dependencies
+require 'src/Dependencies'
 
 function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    math.randomseed(os.time())
+
+    gFonts = {
+        ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+        ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+        ['large'] = love.graphics.newFont('fonts/font.ttf', 32)
+    }
+    love.graphics.setFont(gFonts['small'])
+
+    gTextures = {
+        ['background'] = love.graphics.newImage('graphics/background.png'),
+        ['main'] = love.graphics.newImage('graphics/breakout.png'),
+        ['arrows'] = love.graphics.newImage('graphics/arrows.png'),
+        ['hearts'] = love.graphics.newImage('graphics/hearts.png'),
+        ['particle'] = love.graphics.newImage('graphics/particle.png')
+    }
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+
+        -- set up our sound effects; later, we can just index this table and
+    -- call each entry's `play` method
+    gSounds = {
+        ['paddle-hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall-hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+        ['confirm'] = love.audio.newSource('sounds/confirm.wav', 'static'),
+        ['select'] = love.audio.newSource('sounds/select.wav', 'static'),
+        ['no-select'] = love.audio.newSource('sounds/no-select.wav', 'static'),
+        ['brick-hit-1'] = love.audio.newSource('sounds/brick-hit-1.wav', 'static'),
+        ['brick-hit-2'] = love.audio.newSource('sounds/brick-hit-2.wav', 'static'),
+        ['hurt'] = love.audio.newSource('sounds/hurt.wav', 'static'),
+        ['victory'] = love.audio.newSource('sounds/victory.wav', 'static'),
+        ['recover'] = love.audio.newSource('sounds/recover.wav', 'static'),
+        ['high-score'] = love.audio.newSource('sounds/high_score.wav', 'static'),
+        ['pause'] = love.audio.newSource('sounds/pause.wav', 'static'),
+
+        ['music'] = love.audio.newSource('sounds/music.wav', 'static')
+    }
+
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end
+    }
+    gStateMachine:change('start')
+
+
+    love.keyboard.keysPressed = {}
+end
+
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.update(dt)
+    -- this time, we pass in dt to the state object we're currently using
+    gStateMachine:update(dt)
+
+    -- reset keys pressed
+    love.keyboard.keysPressed = {}
+end
+
+function love.keypressed(key)
+    -- add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+function love.draw()
+    -- begin drawing with push, in our virtual resolution
+    push:apply('start')
+
+    local backgroundWidth = gTextures['background']:getWidth()
+    local backgroundHeight = gTextures['background']:getHeight()
+
+    love.graphics.draw(gTextures['background'], 
+        -- draw at coordinates 0, 0
+        0, 0, 
+        -- no rotation
+        0,
+        -- scale factors on X and Y axis so it fills the screen
+        VIRTUAL_WIDTH / (backgroundWidth - 1), VIRTUAL_HEIGHT / (backgroundHeight - 1))
+
+    gStateMachine:render()
     
+    push:apply('end')
 end
